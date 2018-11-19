f9aa7ca4a0381d6d221e29c666c59107108d87a2
mladenovic_a <mladenovic@comprigo.com>
2018-11-19 11:49:18 +0100

    Add MongoDB url for production

--- src/server/config/constants.js
+++ src/server/config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     'production' : {
-        'mongoURL' : ''
+        'mongoURL' : 'mongodb://ds027771.mlab.com:heroku_l85tzh5n@ds027771.mlab.com:27771/heroku_l85tzh5n'
     },
     'development' : {
         'mongoURL' : 'mongodb://127.0.0.1:27017/ContactList'
