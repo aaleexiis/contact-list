efde6cd0e0e5c1a85df32a6d8da83a54f2c5c8d3
mladenovic_a <mladenovic@comprigo.com>
2018-11-19 12:16:58 +0100

    Change MongoDB production URI

--- src/server/config/constants.js
+++ src/server/config/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
     'production' : {
-        'mongoURL' : 'mongodb://ds027771.mlab.com:heroku_l85tzh5n@ds027771.mlab.com:27771/heroku_l85tzh5n'
+        'mongoURL' : 'mongodb://heroku_l85tzh5n:fr59jbhisftq3h4sgvqbj7u09a@ds027771.mlab.com:27771/heroku_l85tzh5n'
     },
     'development' : {
         'mongoURL' : 'mongodb://127.0.0.1:27017/ContactList'
